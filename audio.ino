5633dfd7ae40d7ba86ab06b6c740f0cb0edf68d4
Bryan Stitt <bryan@stitthappens.com>
2018-06-19 13:12:32 -0700

    it plays wav and aac consistently. picky about mp3

---
+++ audio.ino
@@ -0,0 +1,242 @@
+void setupAudio() {
+  audioShield.enable();
+
+  // TODO: tune this
+  audioShield.volume(0.5);
+
+  // Audio connections require memory to work.  For more
+  // detailed information, see the MemoryAndCpuUsage example
+  // TODO: tune this
+  AudioMemory(20);
+
+  // put the gain a bit lower, some MP3 files will clip otherwise.
+  mixer_left.gain(0, 0.9);
+  mixer_right.gain(0, 0.9);
+
+  mixer_mono.gain(0, 0.5);
+  mixer_mono.gain(1, 0.5);
+
+  // TODO: set line output levels
+}
+
+bool is_hidden_file(String filename) {
+  // TODO: i don't think we need the check for '.'
+  return filename.startsWith('_') or filename.startsWith('.');
+}
+
+bool is_mp3(String filename) {
+  return filename.endsWith(F(".MP3"));
+}
+
+bool is_aac(String filename) {
+  return filename.endsWith(F(".AAC")) or filename.endsWith(F(".MP4")) or filename.endsWith(F(".M4A"));
+}
+
+bool is_wav(String filename) {
+  return filename.endsWith(F(".WAV"));
+}
+
+// TODO: take args so this can load music, meditations, and sound effects
+void loadTracks() {
+  static File root;
+
+  // Starting to index the SD card for MP3/AAC.
+  // TODO: load /music, /meditations, and /noises
+  root = SD.open("/");
+
+  while (true) {
+    File file = root.openNextFile();
+    if (!file) {
+      // If no more files, break out.
+      break;
+    }
+    String file_string = file.name(); // put file in string
+
+    DEBUG_PRINT(F("Found: "));
+    DEBUG_PRINT(file_string);
+
+    if (is_hidden_file(file_string)) {
+      DEBUG_PRINTLN(F(" (Skipped)"));
+
+      file.close();
+      continue;
+    }
+
+    if (is_mp3(file_string)) {
+      DEBUG_PRINTLN(F(" (MP3)"));
+      tracks[num_tracks].codec = TRACK_MP3;
+    } else if (is_aac(file_string)) {
+      DEBUG_PRINTLN(F(" (AAC)"));
+      tracks[num_tracks].codec = TRACK_AAC;
+    } else if (is_wav(file_string)) {
+      DEBUG_PRINTLN(F(" (WAV)"));
+      tracks[num_tracks].codec = TRACK_WAV;
+    } else {
+      DEBUG_PRINTLN(F(" (Skipped)"));
+
+      file.close();
+      continue;
+    }
+
+    // TODO: wth. the .WAV file is getting another name stuck to the end of it
+    strcpy(tracks[num_tracks].filename, file.name());
+
+    file.close();
+
+    DEBUG_PRINT(F("Saved to track list as: #"));
+    DEBUG_PRINT(num_tracks);
+    DEBUG_PRINT(F(" "));
+    DEBUG_PRINTLN(tracks[num_tracks].filename);
+
+    num_tracks++;
+  }
+
+  // make sure EEPROM/current_track have a valid value
+  // TODO: instead of reading/writing to 0, write to an enum/DEFINE
+  current_track = EEPROM.read(0);
+  if (current_track > num_tracks) {
+    EEPROM.write(0, 0);
+    current_track = 0;
+  }
+
+  // TODO: instead of reading/writing to 1, write to an enum/DEFINE
+  play_count = EEPROM.read(1);
+
+  DEBUG_PRINT(F("current_track: "));
+  DEBUG_PRINTLN(current_track);
+
+  DEBUG_PRINT(F("play_count: "));
+  DEBUG_PRINTLN(play_count);
+
+  DEBUG_PRINT(F("num_tracks: "));
+  DEBUG_PRINTLN(num_tracks);
+}
+
+void printAudioSummary() {
+  // print a summary of the current & maximum usage
+  DEBUG_PRINT("CPU: ");
+  DEBUG_PRINT("mp3=");
+  DEBUG_PRINT(player_mp3.processorUsage());
+  DEBUG_PRINT(",");
+  DEBUG_PRINT(player_mp3.processorUsageMax());
+  DEBUG_PRINT("  ");
+  DEBUG_PRINT("aac=");
+  DEBUG_PRINT(player_aac.processorUsage());  // TODO: this always returns 0
+  DEBUG_PRINT(",");
+  DEBUG_PRINT(player_aac.processorUsageMax()); // TODO: this always returns 0
+  DEBUG_PRINT("  ");
+  DEBUG_PRINT("wav=");
+  DEBUG_PRINT(player_wav.processorUsage());
+  DEBUG_PRINT(",");
+  DEBUG_PRINT(player_wav.processorUsageMax());
+  DEBUG_PRINT("  ");
+  DEBUG_PRINT("all=");
+  DEBUG_PRINT(AudioProcessorUsage());
+  DEBUG_PRINT(",");
+  DEBUG_PRINT(AudioProcessorUsageMax());
+  DEBUG_PRINT("    ");
+  DEBUG_PRINT("Memory: ");
+  DEBUG_PRINT(AudioMemoryUsage());
+  DEBUG_PRINT(",");
+  DEBUG_PRINT(AudioMemoryUsageMax());
+  DEBUG_PRINTLN();
+
+  // very simple timing   :-)
+  delay(1000);
+}
+
+int music_play_count = 1;
+
+// pass play_count and current_track by reference so we can support multiple playlists
+void playTrack() {
+  const int track_id = current_track;
+
+  // prepare the next track if we have played this one enough times
+  // TODO: increment after we start playing so we can handle the file failing to play
+  play_count++;
+  DEBUG_PRINT(F("Play count: "));
+  DEBUG_PRINTLN(play_count);
+  if (play_count >= music_play_count) {
+    play_count = 0;
+
+    current_track++;
+    if (current_track >= num_tracks) {
+      current_track = 0;
+    }
+    DEBUG_PRINT(F("Saving next track to EEPROM: "));
+    DEBUG_PRINTLN(current_track);
+    EEPROM.write(0, current_track);
+  }
+  EEPROM.write(1, play_count);
+
+  DEBUG_PRINT(F("Playing track: #"));
+  DEBUG_PRINT(track_id);
+  DEBUG_PRINT(F(" "));
+  DEBUG_PRINT(tracks[track_id].filename);
+
+  // Start playing the file. This sketch continues to run while the file plays.
+  // TODO: dry this up
+  // TODO: play WAV/OGG/FLAC files, too?
+  // TODO: don't wait here so that we can handle lights/sensors well. or atleast call updatelights/sensors in a loop
+  switch (tracks[track_id].codec) {
+  case (TRACK_AAC):
+    DEBUG_PRINTLN(F(" (as AAC)"));
+
+    // TODO: none of my example AAC tracks are playing!
+    player_aac.play(tracks[track_id].filename);
+
+    // A brief delay for the library read file info
+    delay(5);
+
+    // Simple wait for the file to finish playing.
+    while (player_aac.isPlaying())
+      printAudioSummary();
+
+    break;
+  case (TRACK_MP3):
+    DEBUG_PRINTLN(F(" (as MP3)"));
+
+    // TODO: none of my example MP3 tracks are playing!
+    player_mp3.play(tracks[track_id].filename);
+
+    // A brief delay for the library read file info
+    delay(5);
+
+    /*
+    // TODO: retry mp3 as aac?
+    if (! player_mp3.isPlaying()) {
+      play_count--;
+      current_track--;  // TODO: this isn't saved to EEPROM, but that's okay for now
+
+      // the player isn't playing only 5ms after starting
+      // sometimes .MP3 files are actually encoded with AAC
+      tracks[track_id].codec = TRACK_AAC;
+
+      // TODO: rename the file to .AAC for next boot?
+      playTrack();
+      // TODO: this is still failing sometimes
+      return;
+    }
+    */
+
+    // Simple wait for the file to finish playing.
+    // TODO: this is exiting immediatly. try a different file and try wav
+    while (player_mp3.isPlaying())
+      printAudioSummary();
+
+    break;
+  case (TRACK_WAV):
+    DEBUG_PRINTLN(F(" (as WAV)"));
+
+    player_wav.play(tracks[track_id].filename);
+
+    // A brief delay for the library read file info
+    delay(5);
+
+    // Simple wait for the file to finish playing.
+    while (player_wav.isPlaying())
+      printAudioSummary();
+
+    break;
+  }
+}
---
+++ night-sounds.ino
@@ -0,0 +1,112 @@
+// TODO: this plays AAC and WAV files consistently, but MP3 has issues
+
+#define DEBUG
+#include <bs_debug.h>
+
+#include <Audio.h>
+#include <EEPROM.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include <play_sd_aac.h>
+#include <play_sd_mp3.h>
+
+#define SDCARD_CS_PIN 10
+#define SPI_MOSI_PIN 7  // Audio shield has MOSI on pin 7
+#define SPI_SCK_PIN 14  // Audio shield has SCK on pin 14
+#define VOLUME_KNOB A2 // TODO: not attached!
+
+#define MAX_TRACKS 255
+
+// Audio GUI - https://www.pjrc.com/teensy/gui/: begin automatically generated code
+// TODO: wth. only the wav player seems to be working
+AudioPlaySdWav           player_wav;     //xy=152,255
+AudioPlaySdMp3           player_mp3;     //xy=154,197
+AudioPlaySdAac           player_aac;     //xy=154,307
+AudioMixer4              mixer_left;     //xy=415,210
+AudioMixer4              mixer_right;    //xy=420,282
+AudioMixer4              mixer_mono;     //xy=654.1538391113281,249.84613037109375
+AudioOutputI2S           output_mono;    //xy=869,250
+AudioConnection          patchCord1(player_wav, 0, mixer_left, 1);
+AudioConnection          patchCord2(player_wav, 1, mixer_right, 1);
+AudioConnection          patchCord3(player_mp3, 0, mixer_left, 0);
+AudioConnection          patchCord4(player_mp3, 1, mixer_right, 0);
+AudioConnection          patchCord5(player_aac, 0, mixer_left, 2);
+AudioConnection          patchCord6(player_aac, 1, mixer_right, 2);
+AudioConnection          patchCord7(mixer_left, 0, mixer_mono, 0);
+AudioConnection          patchCord8(mixer_right, 0, mixer_mono, 1);
+AudioConnection          patchCord9(mixer_mono, 0, output_mono, 0);
+AudioConnection          patchCord10(mixer_mono, 0, output_mono, 1);
+AudioControlSGTL5000     audioShield;    //xy=147,123
+// Audio GUI - https://www.pjrc.com/teensy/gui/: end automatically generated code
+
+enum TrackCodec: byte {
+  TRACK_AAC, TRACK_MP3, TRACK_WAV
+};
+
+typedef struct {
+  TrackCodec codec;
+  char filename[12];  // 8.3
+} Track;
+
+// TODO: change this to support multiple playlists
+Track tracks[MAX_TRACKS];
+int num_tracks = 0;
+int current_track = 0;
+int play_count = 0;
+
+void setupSPI() {
+  SPI.setMOSI(SPI_MOSI_PIN);
+  SPI.setSCK(SPI_SCK_PIN);
+
+  digitalWrite(SDCARD_CS_PIN, HIGH);
+
+  delay(100); // give everything time to wake up
+
+  SPI.begin();
+}
+
+void setupSD() {
+  // Start SD card
+
+  // TODO: change this to check the sd card in a loop instead of error in a loop
+  while (1) {
+    if (SD.begin(SDCARD_CS_PIN)) {
+      DEBUG_PRINTLN("SD card detected!");
+      break;
+    }
+
+    DEBUG_PRINTLN("Unable to access the SD card");
+    delay(1000);
+  }
+}
+
+void setup() {
+  #ifdef DEBUG
+    Serial.begin(115200);
+
+    delay(1000);
+    while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB
+    }
+  #endif
+
+  DEBUG_PRINTLN("Setting up...");
+
+  setupSPI();
+
+  setupAudio();
+
+  setupSD();
+
+  loadTracks();
+
+  DEBUG_PRINTLN("Starting...");
+}
+
+void loop() {
+  playTrack();
+
+  DEBUG_PRINTLN(F("Looping..."));
+}
